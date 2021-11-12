3f52d8daf02e6794bb50f6032cd44171ec999d6b
FulvioFPimentel <fulviofp@hotmail.com>
2021-11-12 11:12:13 -0300

    Backend project created

---
+++ backend/src/main/java/com/bigcrowd/noticeBoard/NoticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.bigcrowd.noticeBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeBoardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bigcrowd/noticeBoard/NoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigcrowd.noticeBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
